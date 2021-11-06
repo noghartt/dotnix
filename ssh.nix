6f2bd1b33b5c86601b0018abdce2bbbd76b8b2d5
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-06 18:54:01 -0300

    enable ssh program and change path to openssh hasheable folder

--- ssh.nix
+++ ssh.nix
@@ -1,6 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
+  programs.ssh = {
+    enable = true;
+    extraConfig = ''
+      AddKeysToAgent yes
+    '';
+  };
+
   systemd.user.services.ssh-agent = {
     Unit = { Description = "SSH Key Agent"; };
 
@@ -10,7 +17,7 @@
         "SSH_AUTH_SOCK=%t/ssh-agent.socket"
         "DISPLAY=:0"
       ];
-      ExecStart = "/usr/bin/ssh-agent -D -a $SSH_AUTH_SOCK";
+      ExecStart = "${pkgs.openssh}/bin/ssh-agent -D -a $SSH_AUTH_SOCK";
     };
 
     Install = { WantedBy = [ "default.target" ]; };
