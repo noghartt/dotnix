527da7e8ffe7146db0c8aa468dc10c6aa9ae564d
Guilherme Ananias <iamguilhermeananias@gmail.com>
2021-11-14 15:50:16 -0300

    fish

--- fish.nix
+++ fish.nix
@@ -6,13 +6,20 @@
 
     shellInit = ''
       set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+
+      set -x ANDROID_HOME $HOME/Android/Sdk
+      set -gx PATH $ANDROID_HOME/{platform_tools,tools,tools/bin,emulator} $PATH
+
+      set -g theme_display_rw no
+      set -g theme_display_group no
     '';
   };
 
-  home.activation.setFishAsDefaultShell = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    readonly fish_bin_path="${config.home.homeDirectory}/.nix-profile/bin/fish";
-    if ! grep -q "$fish_bin_path" "/etc/shells"; then
-      sudo tee -a /etc/shells <<< "$fish_bin_path"
-    fi
-  '';
+  home.activation.setFishAsDefaultShell =
+    lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+      readonly fish_bin_path="${config.home.homeDirectory}/.nix-profile/bin/fish";
+      if ! grep -q "$fish_bin_path" "/etc/shells"; then
+        sudo tee -a /etc/shells <<< "$fish_bin_path"
+      fi
+    '';
 }
